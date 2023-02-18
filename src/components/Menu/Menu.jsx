633aa400e6962a37a434a418242ab17650f97882
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2023-02-18 14:18:31 +0100

    Update Menu.jsx

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route, NavLink } from 'react-router-dom';
-import Home from 'pages/Home/Home';
-import Movies from 'pages/Movies/Movies';
-import NotFound from 'pages/NotFound/NotFound';
-import MovieDetails from 'pages/MovieDetails/MovieDetails';
-import Cast from 'components/Cast/Cast';
-import Reviews from 'components/Reviews/Reviews';
+import { lazy, Suspense } from 'react';
 
 import styles from './Menu.module.css';
+import Loader from 'components/Loader/Loader';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const NotFound = lazy(() => import('pages/NotFound/NotFound'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 function Menu() {
   return (
@@ -19,19 +22,21 @@ function Menu() {
           Movies
         </NavLink>
       </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies/:id" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:id" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies/:id" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
